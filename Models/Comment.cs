aa412ec5aa01359dd0a6e6e0d1e14da4a9c50ea9
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-10-21 02:34:21 +0500

    Lesson 1: Models

---
+++ Models/Comment.cs
@@ -0,0 +1,20 @@
+namespace Models
+{
+    public class Comment
+    {
+        public int Id { get; set;}
+        public string Title { get; set;} = string.Empty;
+        public string Content { get; set;} = string.Empty;
+        public DateTime CreatedOn {get; set;} = DateTime.Now; 
+
+
+        // Using convention we link Stock-Comment models
+        // Convention means that entity framework dot net core
+        // Search through code and form that relationship (one to many)
+        public int? StockId { get; set; } // ? means can be null
+        public Stock? Stock{ get; set; } // navigation property, allows us to be able to navigate within this relationship
+                                        // so we can go like this Stock.id, Stock.Company and etc.
+        
+
+    }
+}
---
+++ Models/Stock.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18, 2)")] // Purchase attribute, points that in DB it will be saved as decimal accurate to 18 digits and 2 decimal places - 1234567890123456.78 as an example
+        public decimal Purchase { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal LastDiv { get; set; }
+        public string industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+        
+    }
+}
