8eb92843875fefb7f8e2b88a957b4063a1f9ab1c
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-10-26 23:37:57 +0500

    put, delete and readme fix

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -2,6 +2,7 @@ using Data;
 using Dtos.Stock;
 using Mappers;
 using Microsoft.AspNetCore.Mvc;
+using Models;
 
 namespace Controllers
 {
@@ -42,6 +43,41 @@ namespace Controllers
             _context.Stocks.Add(stockModel);
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new {id = stockModel.Id}, stockModel.toStockDto()); // return 201 created
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.industry = updateDto.industry;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.MarketCap = updateDto.MarketCap;
+            stockModel.CompanyName = updateDto.CompanyName;
+
+            _context.SaveChanges();
+            return Ok(stockModel.toStockDto());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if(stockModel == null)
+            {
+                return NotFound();
+            } 
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent(); // 204 status code
         }
     }
 }
